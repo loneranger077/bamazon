7631f8e8f6fb1e2c48fca91af7e8ab2a4ad5b762
nguzak3 <nick.guzak@gmail.com>
2019-05-28 22:02:30 -0400

    initial setup

---
+++ migrations/20190529014626-create-product.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_name: {
+        type: Sequelize.STRING
+      },
+      department_name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      stock_quantity: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Products');
+  }
+};
---
+++ models/product.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    product_name: DataTypes.STRING,
+    department_name: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    stock_quantity: DataTypes.INTEGER
+  }, {});
+  Product.associate = function(models) {
+    // associations can be defined here
+  };
+  return Product;
+};
