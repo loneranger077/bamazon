97cc296bfc9accce7a5d247ba29f06734fde5cb0
nguzak3 <nick.guzak@gmail.com>
2019-06-30 10:12:05 -0400

    deleted extra stuff

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,10 +6,10 @@ var app = express();
 module.exports = function(app) {
 
   app.post("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/api/api.js"));
+    res.sendFile(path.join(__dirname, "/../public/api/api.js"));
   });
 
   app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/api/api.js"));
+      res.sendFile(path.join(__dirname, "/../public/api/api.js"));
   });
 }
